2616afbc1a42b056a21223b034d9933f64e44a88
Tom5521 <mismarttv321@gmail.com>
2023-10-11 23:45:28 -0300

    Improve test script

--- test.sh
+++ test.sh
@@ -9,12 +9,16 @@ echo Accuracy = $accuracy
 
 echo $CalculateIn golang
 ./pi-go $accuracy
+echo 
 echo $CalculateIn python
 python main.py $accuracy
+echo 
 echo $CalculateIn C 
 ./pi-c $accuracy
+echo
 echo $CalculateIn C++
 ./pi-cpp $accuracy
+echo
 echo $CalculateIn Rust
 ./pi-rust $accuracy
 
